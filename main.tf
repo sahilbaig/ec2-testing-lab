84c9bf2b229e43e91200238b0cd545e181237409
Sahil Baig <sahilbaig.me@gmail.com>
2025-03-29 07:43:33 +0530

    step by step

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region     = var.aws_region
 }
@@ -13,36 +14,38 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-   key_name      = "ec2-dev-key"
+  key_name      = "ec2-dev-key" 
+  
   tags = {
     Name    = "EC2_Testing"
     Purpose = "Creating EC2 for testing dev"
   }
   
-  vpc_security_group_ids = [ aws_security_group.ec2-dev-test.id ]
+  vpc_security_group_ids = [aws_security_group.ec2-dev-test.id]
 }
 
-
 resource "aws_security_group" "ec2-dev-test" {
-  name="EC2-Dev-Security-group"
+  name        = "EC2-Dev-Security-group"
   description = "Allow SSH and test ports"
+  
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.ssh_ip}/32"]
+    cidr_blocks = [var.ssh_ip != "" ? "${var.ssh_ip}/32" : "0.0.0.0/0"]
   }
-    ingress {
+  
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    egress {
+  
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
